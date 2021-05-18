c982a4aa49f2287a307a93c588f11f4be5baf8b4
HenriM25 <henri.m.s@hotmail.com>
2021-05-17 22:27:12 -0300

    Header e Footer em php

---
+++ footer.php
@@ -0,0 +1,43 @@
+<footer>
+    <div id="footer">
+  
+      <h1> Exposição Mandrake </h1>
+  
+      <br>
+      
+      <div class="ft">
+        <div id="item">
+          <img src="IMG/email.png"> exposicao@mandrake.com
+        </div>
+  
+        <div id="item">
+          <img src="IMG/suporte.png"> +55 11 0000-0000
+        </div>
+  
+      </div>
+  
+      <div class="Ind">
+  
+        <h4> Mandrake's Industries 2019-2021 </h4>  
+  
+      </div>
+  
+    </div>
+  </footer>
+  <script>
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 480 || document.documentElement.scrollTop > 480) {
+        document.getElementById("menu").style.position="fixed";
+        document.getElementById("menu").style.marginLeft="5%";
+        document.getElementById("menu").style.marginRight="5%";
+      } else {
+        document.getElementById("menu").style.position="sticky";
+        // document.getElementById("menu").style.width="100%";
+      }
+    }
+    </script>
+  
+   </body>
+  </html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include 'header.php'; ?> 
+
+<div id="Page">
+
+    <br>
+
+      <!-- <div class="Filtro col-2">
+        <h1>Filtro</h1>
+        <p>AAAAAA</p>
+        <p>AAAAAA</p>
+      </div> -->
+
+      <div class="Home col-9.5">
+        <h1>HOME</h1>
+      </div>
+
+</div>
+
+<?php include 'footer.php'; ?> 
