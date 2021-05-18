432638378eca7a7c2c2833299f82b30d857b7d27
HenriM25 <henri.m.s@hotmail.com>
2021-05-17 22:46:10 -0300

    Atualização Esculturas e Quadros em php

---
+++ quadros.php
@@ -0,0 +1,165 @@
+<?php include 'header.php'; ?> 
+
+<div id="Page">
+    <div id="Arte">
+        <h1>O que é arte abstrata?</h1>
+<p>A arte abstrata é uma arte não objetiva, ela não precisa de uma representação precisa e objetiva de uma realidade visual, mas possui formas, cores e texturas para gerar seu efeito. A arte abstrata teve seu início no século XX e foi extremamente radical pro seu tempo. Muitos afirmam que a abstração teve incio através de pinturas rupestres de milhares de anos atrás, esse tipo de arte representou uma ruptura artística em relaçao aos moldes tradicionais, sendo essa sua principal característica.</p>
+        
+    <h1>Obras Abstratas</h1>
+
+    <div class="Quadros">
+
+      <div>
+        <img src="IMG/soft.jpg" height="152" width="130"/>
+        <h6>Autor: Wassily Kandinsky </h6>
+        <p> Titulo: Soft duro </p>
+      </div>
+
+      <div>
+        <img src="IMG/linhas.jpg" height="152" width="130"/>
+        <h6> Autor: Wassily Kandinsky </h6>
+        <p> Titulo: Linhas de interseção </p>
+      </div>
+
+      <div>
+        <img src="IMG/elevacao.jpg" height="152" width="130"/>
+        <h6> Autor: Wassily Kandisky </h6>
+        <p> Titulo: Elevação</p>
+      </div>  
+
+      <div>
+        <img src="IMG/constru.jpg" height="152" width="130"/>
+        <h6> Autor:Wassily Kandisky </h6>
+        <p> Titulo:Construção amolecida</p>
+      </div>
+
+      <div>
+        <img src="IMG/composicao.jpg" height="152" width="130"/>
+        <h6> Autor:Wassily Kandisky </h6>
+        <p> Titulo:Composição </p>
+      </div> 
+        
+    </div>
+
+    <br>
+
+    <h1>O que é arte veduta?</h1>
+        <p>A palavra veduta significa "vista" em italiano, esse tipo de arte é altamente detalhada em grande escala pintura ou, na verdade, mais frequentemente de impressão, de uma paisagem urbana ou alguma outra vista. Os pintores dessa arte são chamados de vedutisti. A arte veduta tem origem na Flandres no século XVI</p>
+<h1> Obras Veduta</h1>
+    <div class="Quadros">
+
+      <div>
+        <img src="IMG/seas.png" height="152" width="130"/>
+        <h6> Autor:Paul Bril </h6>
+        <p> Titulo:Seascape </p>
+      </div>
+
+      <div>
+        <img src="IMG/panesrinx.jpg" height="152" width="130"/>
+        <h6> Autor:Paul Bril </h6>
+        <p> Titulo:Pan e Syrinx</p>
+      </div>
+
+      <div>
+        <img src="IMG/ninfas.jpg" height="152" width="130"/>
+        <h6> Autor:Paul Bril </h6>
+        <p> Titulo:Paisagem com Ninfas e Sátiros </p>
+      </div>  
+
+      <div>
+        <img src="IMG/paisagemextensa.jpg" height="152" width="130"/>
+        <h6> Autor:Autor:Paul Bril </h6>
+        <p> Titulo:Uma extensa paisagem </p>
+      </div>
+
+      <div>
+        <img src="IMG/porto.jpg" height="152" width="130"/>
+        <h6> Autor:Autor:Paul Bril </h6>
+        <p> Titulo:o porto </p>
+      </div> 
+        
+    </div>
+
+    <br>
+
+    <h1>O que é arte figurativa?</h1>
+<p>Ao contrario da arte abstrata, a arte figurativa ou figurativismo, é um estilo artístico das artes visuais pautado na representação das formas, seja de seres humanos, objetos, animais, paisagens, entre outros. A arte figurativa remonta séculos de existência humana. As pinturas indicavam a vontade do homem de expressar emoções, sentimentos e de copiar e reproduzir figuras da natureza.</p>
+    <div class="Quadros">
+        <h1>Obras Figurativas</h1>
+
+      <div>
+        <img src="IMG/fruta.jpg" height="152" width="130"/>
+        <h6> Autora:Françoise Adnet </h6>
+        <p> Titulo:Girl With Fruit </p>
+      </div>
+
+      <div>
+        <img src="IMG/kiki.jpg" height="152" width="130"/>
+        <h6> Autora:Françoise Adnet </h6>
+        <p> Titulo:Kiki Au Panier </p>
+      </div>
+
+      <div>
+        <img src="IMG/vaso.jpg" height="152" width="130"/>
+        <h6> Autora:Françoise Adnet </h6>
+        <p> Titulo:Oiseau Et Vases </p>
+      </div>  
+
+      <div>
+        <img src="IMG/pierrot.jpg" height="152" width="130"/>
+        <h6> Autora:Françoise Adnet </h6>
+        <p> Titulo:Mon Ami Pierrot </p>
+      </div>
+
+      <div>
+        <img src="IMG/fillet.jpg" height="152" width="130"/>
+        <h6> Autora:Françoise Adnet </h6>
+        <p> Titulo:Filette </p>
+      </div> 
+        
+    </div>
+
+    <br><br><br><br>
+
+    <h1>O que é arte realista?</h1>
+<p>Arte realista é usada para designar desenhos, esculturas e pinturas que tenham uma representação objetiva da realidade, uma forma de reproduzir a realidade tal como ela é.O objetivo dessa arte era reetratar a realidade do povo de maneira concreta e não uma forma ideal. As artes realistas tiveram inicio na metade do século XIX, na França.</p>
+        <h1>Obras Realistas</h1>
+    <div class="Quadros">
+
+      <div>
+        <img src="IMG/atelier.jpg" height="152" width="130"/>
+        <h6> Autor:Gustave Courbet </h6>
+        <p> Titulo:O Ateliê do Pintor </p>
+      </div>
+
+      <div>
+        <img src="IMG/ferido.jpg" height="152" width="130"/>
+        <h6> Autor:Gustave Courbet </h6>
+        <p> Titulo:O homem ferido </p>
+      </div>
+
+      <div>
+        <img src="IMG/banhista.jpg" height="152" width="130"/>
+        <h6> Autor:Gustave Courbet </h6>
+        <p> Titulo:As Banhistas </p>
+      </div>  
+
+      <div>
+        <img src="IMG/frank.jpg" height="152" width="130"/>
+        <h6> Autor:Gustave Courbet </h6>
+        <p> Titulo:Mulher de Frankfurt </p>
+      </div>
+
+      <div>
+        <img src="IMG/mar.jpg" height="152" width="130"/>
+        <h6> Autor:Gustave Courbet </h6>
+        <p> Titulo:O Mar Tormentoso </p>
+      </div> 
+
+      <br>
+        
+    </div>
+  </div>
+</div>
+
+<?php include 'footer.php'; ?> 
